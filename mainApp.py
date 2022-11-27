a9aec79b40c4b6b5707102a2c6f8a36cfe142134
Turjo47 <turjo47@gmail.com>
2022-11-27 16:57:53 +0600

    database update

--- mainApp.py
+++ mainApp.py
@@ -6,30 +6,25 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMainWindow,QApplication,QLabel,QPushButton,QTextBrowser,QMenuBar,QMenu,QAction,QStatusBar,QLineEdit,QWidget,QDialog,QTableWidget
 from PyQt5.uic import loadUi
 
-con = sqlite3.connect('attendence.bd')
-cur = con.cursor()
-cur.execute('''CREATE TABLE IF NOT EXISTS infos(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            ip TEXT,
-            port TEXT,
-            api TEXT
-            )''')
-            # # con.commit()
-con.commit()
-con.close()
-
-
 
 class ServerWindow(QMainWindow):
     def __init__(self):
         super(ServerWindow,self).__init__()
         loadUi("localserver.ui",self)
-        self.actionAdd_New.triggered.connect(self.displayinfo)
-        self.displayinfo()
+        self.actionAdd_New.triggered.connect(self.add_bd)
+        self.add_bd()
         
-    def displayinfo(self):
+    def add_bd(self):
         widget.setFixedSize(400,180)
-        
+        # con = sqlite3.connect('attendence.bd')
+        # cur = con.cursor()
+        # cur.execute('''CREATE TABLE IF NOT EXISTS infos(
+        #     id INTEGER PRIMARY KEY AUTOINCREMENT,
+        #     ip TEXT,
+        #     port TEXT,
+        #     api TEXT
+        #     )''')
+        # con.commit()
         widget.setCurrentIndex(widget.currentIndex()+1)
         self.show()
     
@@ -38,31 +33,39 @@ class InputWindow(QMainWindow):
         super(InputWindow,self).__init__()
         loadUi("input.ui",self)
         # self.serverwindow=ServerWindow()
-        # Ip= self.Ip_lineEdit.text()
-        # Port=self.Port_lineEdit.text()
-        # Api=self.Api_lineEdit.text()
+        Ip= self.Ip_lineEdit.text()
+        Port=self.Port_lineEdit.text()
+        Api=self.Api_lineEdit.text()
         self.okButton.clicked.connect(self.passinfo)
-        self.saveButton.clicked.connect(self.save_it)
+        self.saveButton.clicked.connect(self.add_database)
+        self.add_database()
         # self.serverwindow = ServerWindow()
     def passinfo(self):
         widget.setFixedSize(400,450)
+        
         widget.setCurrentIndex(widget.currentIndex()-1)
         # self.serverwindow.displayinfo()
-    def save_it(self):
-        con = sqlite3.connect('attendence.bd')
-        cur = con.cursor() 
-        users =[]
-        for index in range(self.Ip_lineEdit):
-            users.append(self.Ip_llineEdit.users(index))
-        for user in users:
-            cur.execute("INSERT INTO infos VALUES(:user)",
-                       {
-                        'user':user.text(),
-                        }
-                        )
+    def add_database(self):
+        try:
+            con = sqlite3.connect('attendence.bd')
+            cur = con.cursor()
+            users= cur.execute('''CREATE TABLE is not exists infos(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ip TEXT NOT NULL,
+            port TEXT NOT NULL,
+            api TEXT NOT NULL
+            )''')
+            # # con.commit()
+            sql= """INSERT INTO infos(ip,port,api) VALUES (?,?,?)"""
+            cur.execute(sql, users)
+            cur.execute("SELECT rowwid,* FORM infos")
+            items = cur.fetchall()
+            for item in items:
+                print(items)
             con.commit()
             con.close()
-       
+        except:
+            pass
 #main
 app = QApplication(sys.argv)
 widget= QtWidgets.QStackedWidget()
--- testdata.py
+++ testdata.py
@@ -1,12 +1,14 @@
 import sqlite3
-
-
-
 con= sqlite3.connect('data.bd')
-cur= con.cursor
-cur.execute('''CREATE TABLE IF NOT EXISTS
-            ( name TEXT,
-            number TEXT
-                ''')
-cur.execute('''INSERT INTO tables('raha','12876543')''')
-con.commit
+cur= con.cursor()
+cur.execute("""CREATE TABLE tables(
+            name TEXT,
+            number TEXT)
+            """)
+cur.execute("INSERT INTO tables VALUES('raha','12876543')")
+cur.execute("SELECT rowid,* FROM tables")
+items= cur.fetchall()
+for item in items:
+    print (item)
+con.commit()
+con.close()
